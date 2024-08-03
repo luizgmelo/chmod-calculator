8964012b016dd01dd1089d059539d43a705b78f9
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-08-03 09:08:40 -0300

    refac: switch cases for permissions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const userInput = document.querySelector("#c-oct");
 
 const calculatePermissions = () => {
 
+  // parse string to octal
   const owner = parseInt(userInput.value[0], 8);
   const group = parseInt(userInput.value[1], 8);
   const others = parseInt(userInput.value[2], 8);
@@ -11,6 +12,7 @@ const calculatePermissions = () => {
     return;
   } 
 
+  // parse octal to binary
   const ownerBinary = parseInt(owner).toString(2);
   const groupBinary = parseInt(group).toString(2);
   const othersBinary = parseInt(others).toString(2);
@@ -30,136 +32,54 @@ const calculatePermissions = () => {
   const othersWrite = checkboxes[5];
   const othersExecute = checkboxes[8];
 
-  switch (ownerBinary) {
+  switchPerm(ownerBinary, ownerRead, ownerWrite, ownerExecute);
+  switchPerm(groupBinary, groupRead, groupWrite, groupExecute);
+  switchPerm(othersBinary, othersRead, othersWrite, othersExecute);
+}
+
+const switchPerm = (userTypeBinary, userRead, userWrite, userExecute) => {
+  switch (userTypeBinary) {
     case '0':
-      ownerRead.checked = false;
-      ownerWrite.checked = false;
-      ownerExecute.checked = false;
+      userRead.checked = false;
+      userWrite.checked = false;
+      userExecute.checked = false;
       break;
     case '1':
-      ownerRead.checked = false;
-      ownerWrite.checked = false;
-      ownerExecute.checked = true;
+      userRead.checked = false;
+      userWrite.checked = false;
+      userExecute.checked = true;
       break;
     case '10':
-      ownerRead.checked = false;
-      ownerWrite.checked = true;
-      ownerExecute.checked = false;
+      userRead.checked = false;
+      userWrite.checked = true;
+      userExecute.checked = false;
       break;
     case '11':
-      ownerRead.checked = false;
-      ownerWrite.checked = true;
-      ownerExecute.checked = true;
+      userRead.checked = false;
+      userWrite.checked = true;
+      userExecute.checked = true;
       break;
     case '100':
-      ownerRead.checked = true;
-      ownerWrite.checked = false;
-      ownerExecute.checked = false;
+      userRead.checked = true;
+      userWrite.checked = false;
+      userExecute.checked = false;
       break;
     case '101':
-      ownerRead.checked = true;
-      ownerWrite.checked = false;
-      ownerExecute.checked = true;
+      userRead.checked = true;
+      userWrite.checked = false;
+      userExecute.checked = true;
       break;
     case '110':
-      ownerRead.checked = true;
-      ownerWrite.checked = true;
-      ownerExecute.checked = false;
+      userRead.checked = true;
+      userWrite.checked = true;
+      userExecute.checked = false;
       break;
     case '111':
-      ownerRead.checked = true;
-      ownerWrite.checked = true;
-      ownerExecute.checked = true;
+      userRead.checked = true;
+      userWrite.checked = true;
+      userExecute.checked = true;
       break;
   }
-  
-  switch (groupBinary) {
-    case '0':
-      groupRead.checked = false;
-      groupWrite.checked = false;
-      groupExecute.checked = false;
-      break;
-    case '1':
-      groupRead.checked = false;
-      groupWrite.checked = false;
-      groupExecute.checked = true;
-      break;
-    case '10':
-      groupRead.checked = false;
-      groupWrite.checked = true;
-      groupExecute.checked = false;
-      break;
-    case '11':
-      groupRead.checked = false;
-      groupWrite.checked = true;
-      groupExecute.checked = true;
-      break;
-    case '100':
-      groupRead.checked = true;
-      groupWrite.checked = false;
-      groupExecute.checked = false;
-      break;
-    case '101':
-      groupRead.checked = true;
-      groupWrite.checked = false;
-      groupExecute.checked = true;
-      break;
-    case '110':
-      groupRead.checked = true;
-      groupWrite.checked = true;
-      groupExecute.checked = false;
-      break;
-    case '111':
-      groupRead.checked = true;
-      groupWrite.checked = true;
-      groupExecute.checked = true;
-      break;
-  }
-
-  switch (othersBinary) {
-    case '0':
-      othersRead.checked = false;
-      othersWrite.checked = false;
-      othersExecute.checked = false;
-      break;
-    case '1':
-      othersRead.checked = false;
-      othersWrite.checked = false;
-      othersExecute.checked = true;
-      break;
-    case '10':
-      othersRead.checked = false;
-      othersWrite.checked = true;
-      othersExecute.checked = false;
-      break;
-    case '11':
-      othersRead.checked = false;
-      othersWrite.checked = true;
-      othersExecute.checked = true;
-      break;
-    case '100':
-      othersRead.checked = true;
-      othersWrite.checked = false;
-      othersExecute.checked = false;
-      break;
-    case '101':
-      othersRead.checked = true;
-      othersWrite.checked = false;
-      othersExecute.checked = true;
-      break;
-    case '110':
-      othersRead.checked = true;
-      othersWrite.checked = true;
-      othersExecute.checked = false;
-      break;
-    case '111':
-      othersRead.checked = true;
-      othersWrite.checked = true;
-      othersExecute.checked = true;
-      break;
-  }
-
-
 }
 
 
